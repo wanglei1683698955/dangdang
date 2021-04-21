152732a791d7feea74e2ecc0f35914c64b071c1d
wanglei1683698955 <1683698955@qq.com>
2021-04-21 15:54:11 +0800

    更新项目描述与sql文件

---
+++ res/dangdang develop a document.sql
@@ -0,0 +1,119 @@
+﻿# Host: localhost  (Version: 5.5.53)
+# Date: 2021-04-21 15:48:31
+# Generator: MySQL-Front 5.3  (Build 4.234)
+
+/*!40101 SET NAMES utf8 */;
+
+#
+# Structure for table "goodsinfo"
+#
+
+DROP TABLE IF EXISTS `goodsinfo`;
+CREATE TABLE `goodsinfo` (
+  `goodsId` varchar(10) NOT NULL,
+  `goodsName` varchar(100) DEFAULT NULL,
+  `typeId` char(3) NOT NULL,
+  `goodsPrice` float DEFAULT NULL,
+  `goodsCount` int(11) DEFAULT NULL,
+  `goodsDesc` varchar(100) DEFAULT NULL,
+  `goodsImg` varchar(100) DEFAULT NULL,
+  `beiyong1` varchar(100) DEFAULT NULL,
+  `beiyong2` varchar(100) DEFAULT NULL,
+  `beiyong3` varchar(100) DEFAULT NULL,
+  `beiyong4` varchar(100) DEFAULT NULL,
+  `beiyong5` varchar(100) DEFAULT NULL,
+  `beiyong6` varchar(100) DEFAULT NULL,
+  `beiyong7` varchar(100) DEFAULT NULL,
+  `beiyong8` varchar(100) DEFAULT NULL,
+  `beiyong9` varchar(100) DEFAULT NULL,
+  `beiyong10` varchar(100) DEFAULT NULL,
+  `beiyong11` varchar(100) DEFAULT NULL,
+  `beiyong12` varchar(100) DEFAULT NULL,
+  `beiyong13` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`goodsId`),
+  KEY `typeId` (`typeId`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+#
+# Data for table "goodsinfo"
+#
+
+/*!40000 ALTER TABLE `goodsinfo` DISABLE KEYS */;
+/*!40000 ALTER TABLE `goodsinfo` ENABLE KEYS */;
+
+#
+# Structure for table "goodstype"
+#
+
+DROP TABLE IF EXISTS `goodstype`;
+CREATE TABLE `goodstype` (
+  `typeid` char(3) NOT NULL,
+  `goodstype` varchar(20) NOT NULL,
+  PRIMARY KEY (`typeid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+#
+# Data for table "goodstype"
+#
+
+/*!40000 ALTER TABLE `goodstype` DISABLE KEYS */;
+/*!40000 ALTER TABLE `goodstype` ENABLE KEYS */;
+
+#
+# Structure for table "reg"
+#
+
+DROP TABLE IF EXISTS `reg`;
+CREATE TABLE `reg` (
+  `name` char(6) NOT NULL,
+  `password` char(6) NOT NULL,
+  `vip` char(1) DEFAULT '0',
+  PRIMARY KEY (`name`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+#
+# Data for table "reg"
+#
+
+/*!40000 ALTER TABLE `reg` DISABLE KEYS */;
+INSERT INTO `reg` VALUES ('','',''),('111111','111aaa','0'),('123456','123456','0'),('222222','222aaa','0'),('333333','333aaa','0'),('444444','444aaa','0'),('555555','555aaa','0'),('666666','666aaa','1'),('777777','777aaa','0'),('888888','888aaa','0'),('999999','999aaa','1');
+/*!40000 ALTER TABLE `reg` ENABLE KEYS */;
+
+#
+# Structure for table "shoppingcart"
+#
+
+DROP TABLE IF EXISTS `shoppingcart`;
+CREATE TABLE `shoppingcart` (
+  `vipName` varchar(10) DEFAULT NULL,
+  `goodsId` varchar(10) DEFAULT NULL,
+  `goodsCount` int(11) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+#
+# Data for table "shoppingcart"
+#
+
+/*!40000 ALTER TABLE `shoppingcart` DISABLE KEYS */;
+/*!40000 ALTER TABLE `shoppingcart` ENABLE KEYS */;
+
+#
+# Structure for table "vip"
+#
+
+DROP TABLE IF EXISTS `vip`;
+CREATE TABLE `vip` (
+  `username` varchar(20) NOT NULL,
+  `userpass` varchar(16) NOT NULL,
+  `sex` char(2) DEFAULT '女',
+  `age` int(11) DEFAULT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+#
+# Data for table "vip"
+#
+
+/*!40000 ALTER TABLE `vip` DISABLE KEYS */;
+INSERT INTO `vip` VALUES ('666666','666666','女',18);
+/*!40000 ALTER TABLE `vip` ENABLE KEYS */;
